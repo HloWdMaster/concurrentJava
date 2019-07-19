44ec0d7967a384a3d748c400f057e7780c0a7fba
MinNong <1457391646@qq.com>
2019-07-19 23:46:33 +0800

    同步异步查找文件名

--- chapter01/src/main/java/file_search/FileSearchMain.java
+++ chapter01/src/main/java/file_search/FileSearchMain.java
@@ -12,23 +12,23 @@ import java.util.Date;
 public class FileSearchMain {
     public static void main(String[] args) {
         Date start1 = new Date();
-        SerialFileSearch.searchFile(new File("D:\\AppServ"),"phpinfo.php",new Result());
+        SerialFileSearch.searchFile(new File("D:\\S7edge"),"phpinfo.php",new Result());
         Date end1 = new Date();
         System.out.printf("Serial cost: %d%n",end1.getTime()-start1.getTime());
 
         Date start2 = new Date();
-        ParallelGroupFileSearch.searchFile(new File("D:\\AppServ"),"phpinfo.php",new Result());
+        ParallelGroupFileSearch.searchFile(new File("D:\\S7edge"),"phpinfo.php",new Result());
         Date end2 = new Date();
         System.out.printf("Parallel cost: %d%n",end2.getTime()-start2.getTime());
 
 
         Date start11 = new Date();
-        SerialFileSearch.searchFile(new File("D:\\AppServ"),"java.php",new Result());
+        SerialFileSearch.searchFile(new File("D:\\S7edge"),"360手机助手导出的联系人.xml",new Result());
         Date end11 = new Date();
         System.out.printf("Serial cost: %d%n",end1.getTime()-start1.getTime());
 
         Date start21 = new Date();
-        ParallelGroupFileSearch.searchFile(new File("D:\\AppServ"),"java.php",new Result());
+        ParallelGroupFileSearch.searchFile(new File("D:\\S7edge"),"360手机助手导出的联系人.xml",new Result());
         Date end21 = new Date();
         System.out.printf("Parallel cost: %d%n",end2.getTime()-start2.getTime());
     }
